dfce8ebf7226ee0eae5a321dff3f98ef7555abcb
CinemaAddict <brokenswords21@gmail.com>
2024-02-13 13:23:17 +0300

    adding origin to cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,10 @@ export default class Server {
 
   private config(app: Application): void {
     const corsOptions: CorsOptions = {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://ocean-goods-client.vercel.app",
+      ],
       credentials: true,
     };
 
